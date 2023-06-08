d9ce32e55e8292169ea9c5fc9d6bee8acae1a757
Brian Rogers <brian@th3rogers.com>
2023-06-07 23:23:33 -0700

    initial commit

---
+++ main.tf
@@ -0,0 +1,8 @@
+provider "oci" {
+  tenancy_ocid     = "your_tenancy_ocid"
+  user_ocid        = "your_user_ocid"
+  fingerprint      = "your_fingerprint"
+  private_key_path = "path/to/your/private/key"
+  region           = "phx"
+}
+
